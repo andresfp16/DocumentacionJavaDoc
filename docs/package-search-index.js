1b47d48ee9076acc037e401d008067d0f853191e
andresfp16 <91657281+andresfp16@users.noreply.github.com>
2022-05-12 18:06:12 +0200

    Delete docs directory

--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Gestisimal"},{"l":"Gestisimal.Excepciones"},{"l":"Gestisimal.Negocio"},{"l":"Gestisimal.presentacion"}];updateSearchResults();
